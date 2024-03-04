8beed1d8118648832862cea9fa90dd540714ee36
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-03-04 13:15:13 +0300

    Update SimpleWine.py

--- WebScraping/SimpleWine.py
+++ WebScraping/SimpleWine.py
@@ -32,3 +32,22 @@ class SimpleWineParser:
         items = soup.find_all('article', class_='snippet swiper-slide')
         wines = []
     
+        for item in items:
+            description_items = item.find_all('div', class_='snippet-description__item')
+
+            keys_list = [description_item.get_text(strip=True).split(':')[0] for description_item in description_items]
+            values_list = [description_item.find('a').get_text(strip=True) if description_item.find('a') else description_item.find('span').get_text(strip=True) for description_item in description_items]
+
+            description_data = dict(zip(keys_list, values_list))
+
+            title = item.find('div', class_='snippet-middle').find('a', class_='snippet-name js-dy-slot-click').get_text(strip=True)
+            link_product = item.find('div', class_='snippet-middle').find('a').get('href')
+
+            ratings_span = item.find('span', class_='snippet-star__value')
+            ratings = ratings_span.get_text(strip=True) if ratings_span else None
+
+            votes_span = item.find('span', class_='snippet-star__reviews')
+            number_votes = ''.join(filter(str.isdigit, votes_span.get_text(strip=True))) if votes_span else None
+
+            price_div = item.find('div', class_='snippet-price__total')
+            common_price = item.find('span', class_='snippet-price__total snippet-price__total-black')
