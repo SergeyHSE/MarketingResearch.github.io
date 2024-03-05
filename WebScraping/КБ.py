d876c00757fc108bf1ecb682a0fb10a5f27ebbde
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-03-05 11:24:42 +0300

    Create КБ.py

---
+++ WebScraping/КБ.py
@@ -0,0 +1,13 @@
+"""
+Created on Fri Jan 26 19:35:18 2024
+
+@author: User
+"""
+
+import csv
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
