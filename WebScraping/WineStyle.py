89011737f1ce6682eaf59d0e8dd61ef7f0317636
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-03-06 16:42:49 +0300

    Update WineStyle.py

--- WebScraping/WineStyle.py
+++ WebScraping/WineStyle.py
@@ -10,3 +10,34 @@ import csv
 import re
 import time
 
+def get_content(html):
+    soup = BeautifulSoup(html.text, 'html.parser')
+    items = soup.find_all('form', class_='item-block')
+    wines = []
+
+    for item in items:
+
+        title = item.find('p', class_='title').find('a').get_text(strip=True)
+
+        price = item.find('div', class_='price').get_text(strip=True)
+        price_digits = re.sub(r'\D', '', price)
+              
+        rating_div = item.find('div', class_='info-block rating-text')
+        ratings = rating_div.find('span', class_='text').get_text(strip=True) if rating_div else None
+        
+        number_votes = rating_div.find('span', class_='link-reviews').get_text(strip=True) if rating_div else None
+        
+        desc_items = item.find('ul', class_='list-description').find_all('li')
+        def extract_text_from_elements(elements):
+            return ', '.join(element.get_text(strip=True) for element in elements)
+        
+        keys_list = [desc_item.find('span', class_='name').get_text(strip=True) for desc_item in desc_items]
+        values_list = [extract_text_from_elements(desc_item.find_all('a')) for desc_item in desc_items]
+        description_data = dict(zip(keys_list, values_list))   
+        
+        link_product = item.find('p', class_='title').find('a').get('href')
+        
+        image = item.find('a', class_='img-block').find('img').get('src')
+        
+        volume_element = item.find('span', class_='volume-block__title')
+        volume_text = volume_element.next_sibling.string.strip()
