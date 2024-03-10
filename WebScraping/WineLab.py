b6532e2972cd625f150b8ede1aca7884649bd6a3
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-03-10 10:25:59 +0300

    Create WineLab.py

---
+++ WebScraping/WineLab.py
@@ -0,0 +1,13 @@
+"""
+Created on Fri Feb  2 20:04:54 2024
+
+@author: User
+"""
+
+import json
+import csv
+
+# Specify the paths to your JavaScript files
+path_page1 = r"C:\Users\User\Documents\pyton-projects\spider\Машинное обучение\WebScraping\file.js"
+path_page2 = r"C:\Users\User\Documents\pyton-projects\spider\Машинное обучение\WebScraping\Page2WineLab.js"
+
